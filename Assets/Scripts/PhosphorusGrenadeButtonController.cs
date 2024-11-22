72ec996bf12be644b283b6ffb85fcb69844e8419
Eduardo Salinas <eduardoluis93@gmail.com>
2024-11-22 17:32:47 +0000

    Added the PhosphorousGrenadeButtonController.cs Script. It seems that this script may need some modifications.

---
+++ Assets/Scripts/PhosphorusGrenadeButtonController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class PhosphorusGrenadeButtonController : MonoBehaviour
+    {
+        public GameObject phosphorusGrenadeButton;  // Reference to the Phosphorus grenade button
+        public PhosphorusGrenadeThrowHandler _PhosphorusGrenadeThrowHandler;  // Updated reference
+
+        private bool isPhosphorusGrenadeThrown = false;
+
+        private void Start()
+        {
+            if (phosphorusGrenadeButton != null)
+            {
+                phosphorusGrenadeButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(OnPhosphorusGrenadeButtonPressed);
+            }
+            else
+            {
+                Debug.LogError("Phosphorus grenade button reference not assigned!");
+            }
+        }
+
+        public void OnPhosphorusGrenadeButtonPressed()
+        {
+            if (isPhosphorusGrenadeThrown)
+            {
+                return;
+            }
+            else
+            {
+                isPhosphorusGrenadeThrown = true;
+                StartCoroutine(_PhosphorusGrenadeThrowHandler.PlayTwinTurboDownThenPhosphorousGrenadeThrow());
+            }
+        }
+
+        public void ResetPhosphorusGrenadeThrow()
+        {
+            Debug.Log("ResetPhosphorusGrenadeThrow method invoked.");
+            isPhosphorusGrenadeThrown = false;
+        }
+    }
+}
+
+
+
